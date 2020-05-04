fa2a8beb24cc3c209c2cc070effecd6f8725df65
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2020-05-04 18:30:58 -0300

    Migração de dados pos middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,6 +5,18 @@ module.exports = (req ,res, next) => {
         if(!user){
             return res.status(400).json({ error: 'Usuario não logado!' });
         }
+
+        req.uid = user.uid;
+        req.email = user.email;
+
+        firebase.database().ref('users/' + user.uid).once('value').then(function(snapshot){
+            req.name = snapshot.val().name;
+            req.address = snapshot.val().address;
+            req.number = snapshot.val().number;
+            req.ddd = snapshot.val().ddd;
+            req.phone = snapshot.val().phone;
+        })
+        
         return next();
     });
 }
